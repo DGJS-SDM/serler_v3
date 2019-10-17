8f37e53be3bfcdd969b466771e29628eb95d5603
thienle282 <thienle282@gmail.com>
2019-10-17 15:28:10 +1300

    Update UI for Article Detail and validation for Import Bibtex

--- FrontEnd/src/components/screens/ArticleDetails.js
+++ FrontEnd/src/components/screens/ArticleDetails.js
@@ -6,12 +6,14 @@ import Footer from "../commons/Footer";
 class ArticleDetails extends React.Component {
   constructor(props) {
     super(props);
+
+    //david: declare the state of the article field
     this.state = {
       title: "",
       authors: "",
       semethod: "",
       semethodology: "",
-      researhQuestion: "",
+      researchQuestion: "",
       researchResult: "",
       publication_type: "",
       publication: "",
@@ -22,18 +24,20 @@ class ArticleDetails extends React.Component {
     };
   }
 
+  //david: call API to get article details by id
   componentDidMount() {
     axios
       .get(
         "http://localhost:3001/articles/detail/" + this.props.match.params.id
       )
       .then((response) => {
+        //david: get value of all the fields from database and set for the state
         this.setState({
           title: response.data.article_title,
           authors: response.data.article_authors,
           semethod: response.data.article_seMethod,
           semethodology: response.data.article_seMethodology,
-          researhQuestion: response.data.article_research_question,
+          researchQuestion: response.data.article_research_question,
           researchResult: response.data.article_researchResult,
           publication_type: response.data.article_publication_type,
           publication: response.data.article_publication,
@@ -48,6 +52,7 @@ class ArticleDetails extends React.Component {
         console.log(error);
       });
   }
+  //david: display article details by assign all the states to the htlm tag
   render() {
     return (
       <div className="container">
@@ -56,36 +61,18 @@ class ArticleDetails extends React.Component {
           <li className="collection-header">
             <h4>{this.state.title}</h4>
           </li>
+
           <table>
+            <tr>
+              <td colspan="2">
+                <h6 className="thick-blue">General Information</h6>
+              </td>
+            </tr>
             <tr>
               <td>
                 <span className="thick">Author</span>
               </td>
               <td>{this.state.authors}</td>
-            </tr>
-            <tr>
-              <td>
-                <span className="thick">SE Method:</span>
-              </td>
-              <td>{this.state.semethod}</td>
-            </tr>
-            <tr>
-              <td>
-                <span className="thick">SE Methodology:</span>
-              </td>
-              <td>{this.state.semethodology}</td>
-            </tr>
-            <tr>
-              <td>
-                <span className="thick">Research Question:</span>
-              </td>
-              <td>{this.state.researchQuestion}</td>
-            </tr>
-            <tr>
-              <td>
-                <span className="thick">Research Result:</span>
-              </td>
-              <td>{this.state.researchResult}</td>
             </tr>
             <tr>
               <td>
@@ -116,6 +103,36 @@ class ArticleDetails extends React.Component {
                 <span className="thick">Rating:</span>
               </td>
               <td>{this.state.rating}</td>
+            </tr>
+
+            <tr>
+              <td colspan="2">
+                <h6 className="thick-blue">Analysis Information</h6>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <span className="thick">SE Method:</span>
+              </td>
+              <td>{this.state.semethod}</td>
+            </tr>
+            <tr>
+              <td>
+                <span className="thick">SE Methodology:</span>
+              </td>
+              <td>{this.state.semethodology}</td>
+            </tr>
+            <tr>
+              <td>
+                <span className="thick">Research Question:</span>
+              </td>
+              <td>{this.state.researchQuestion}</td>
+            </tr>
+            <tr>
+              <td>
+                <span className="thick">Research Result:</span>
+              </td>
+              <td>{this.state.researchResult}</td>
             </tr>
           </table>
         </ul>
--- FrontEnd/src/components/screens/ImportBibtex.js
+++ FrontEnd/src/components/screens/ImportBibtex.js
@@ -9,78 +9,136 @@ class ImportBibtex extends React.Component {
     super(props);
     this.readBibFile = this.readBibFile.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+    //david: declare the state of the article field
     this.state = {
-      article_title: "",
-      article_authors: "",
-      article_publication_type: "",
-      article_publication: "",
-      article_doi: "",
-      article_year: "",
-      article_location: "",
-      article_method: "",
-      article_status: "",
-      article_rating: ""
+      title: "",
+      authors: "",
+      publication_type: "",
+      publication: "",
+      doi: "",
+      year: "",
+      location: "",
+      status: "",
+      rating: "",
+      //isActive to check if the bib text file is selected
+      isActive: false
     };
   }
-
+  //david: read the bib file
   readBibFile(e) {
     let _input = null;
     let files = e.target.files;
     let reader = new FileReader();
+
     reader.readAsText(files[0]);
+
     reader.onload = (e) => {
       _input = e.target.result;
+      //david: part the conent from the bibtex file to json format
       var sample = BibtexParse.toJSON(_input);
       var tags = sample[0].entryTags;
-      this.setState({ article_title: tags.title });
-      this.setState({ article_authors: tags.author });
-      this.setState({ article_publication_type: tags.journal });
-      this.setState({ article_publication: tags.publisher });
-      this.setState({ article_doi: tags.volume });
-      this.setState({ article_year: tags.year });
+      //david: get all the elements from the json format and set for the states
+      this.setState({ title: tags.title });
+      this.setState({ authors: tags.author });
+      this.setState({ publication_type: tags.journal });
+      this.setState({ publication: tags.publisher });
+      this.setState({ doi: tags.volume });
+      this.setState({ year: tags.year });
+      this.setState({ isActive: true });
     };
   }
-
+  //david: handle the onSubmit event
   onSubmit(e) {
-    e.preventDefault();
-    const obj = {
-      article_title: this.state.article_title,
-      article_authors: this.state.article_authors,
-      article_publication_type: this.state.article_publication_type,
-      article_publication: this.state.article_publication,
-      article_doi: this.state.article_doi,
-      article_year: this.state.article_year,
-      article_location: "N/A",
-      article_method: "N/A",
-      article_status: "on_hold",
-      article_rating: "0"
-    };
-    axios.post("http://localhost:3001/articles/add", obj);
-    var preview = document.getElementById("show-text");
-    preview.innerHTML = "<span>Article has been added</span>";
-    //.then((res) => console.log(res.data));
+    if (this.state.isActive == false) {
+      var preview = document.getElementById("show-text");
+      preview.innerHTML = "<span>Please select a Bibtex file</span>";
+    } else {
+      e.preventDefault();
+      const obj = {
+        article_title: this.state.title,
+        article_authors: this.state.authors,
+        article_publication_type: this.state.publication_type,
+        article_publication: this.state.publication,
+        article_doi: this.state.doi,
+        article_year: this.state.year,
+        article_location: "N/A",
+        article_status: "on_hold",
+        article_rating: "0"
+      };
+      //david: call the api to add article to database
+      axios.post("http://localhost:3001/articles/add", obj);
+      this.setState({
+        title: "",
+        authors: "",
+        publication_type: "",
+        publication: "",
+        doi: "",
+        year: "",
+        location: "",
+        status: "",
+        rating: ""
+      });
+      //david: inform user the article is added
+      var preview = document.getElementById("show-ms");
+      preview.innerHTML = "<span>Article has been added</span>";
+    }
   }
 
   render() {
     return (
-      <div>
+      <div className="container">
         <Header title="Upload Bibtex File" />
         <input
           className="btn"
           type="file"
+          accept=".bib"
           onChange={(e) => this.readBibFile(e)}
         />
         <div id="show-text"> Choose Bibtex file to import</div>
-        <h1>Preview</h1>
-        <p>Title: {this.state.article_title}</p>
-        <p>Author: {this.state.article_authors}</p>
-        <p>Type: {this.state.article_publication_type}</p>
-        <p>Publiccation: {this.state.article_publication}</p>
-        <p>DOI: {this.state.article_doi}</p>
-        <p>Year: {this.state.article_year}</p>
+        {/* David: Load the value from bibtex file for user to preview */}
+        <h4>Preview</h4>
+        <table>
+          <tr>
+            <td>
+              <span className="thick">Title:</span>
+            </td>
+            <td>{this.state.title}</td>
+          </tr>
+          <tr>
+            <td>
+              <span className="thick">Author:</span>
+            </td>
+            <td>{this.state.authors}</td>
+          </tr>
+          <tr>
+            <td>
+              <span className="thick">Publication Type:</span>
+            </td>
+            <td>{this.state.publication_type}</td>
+          </tr>
+          <tr>
+            <td>
+              <span className="thick">Publication:</span>
+            </td>
+            <td>{this.state.publication}</td>
+          </tr>
+          <tr>
+            <td>
+              <span className="thick">DOI:</span>
+            </td>
+            <td>{this.state.doi}</td>
+          </tr>
+          <tr>
+            <td>
+              <span className="thick">Year:</span>
+            </td>
+            <td>{this.state.year}</td>
+          </tr>
+        </table>
         <button className="btn" onClick={this.onSubmit}>
           Submit
         </button>
+        <div id="show-ms"></div>
         <Footer />
       </div>
     );
