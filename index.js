6fbbdd7cae6835f98193a87d06fdf5b5391ee99b
GhostyNZ_13 <gavinj.millar@gmail.com>
2019-10-19 14:24:44 +1300

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(logger('dev'));
 const articlesRouter = require('./routes/articles');
 app.use('/articles', articlesRouter);
 
-app.get("*, (req, res) => {
+app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 };
 
