10e4ef7c3d1b94d769e5e18531d147c829491107
GhostyNZ_13 <gavinj.millar@gmail.com>
2019-10-20 23:49:50 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const request = require('request');
 var cors = require('cors');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
@@ -33,25 +32,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
-
 const articlesRouter = require('./routes/articles');
 app.use('/articles', articlesRouter);
 
 app.get("*", (req, res) => {
-	request(
-    { url: 'https://https://serler-v3-stage.herokuapp.com/articles' },
-    (error, response, body) => {
-      if (error || response.statusCode !== 200) {
-        return res.status(500).json({ type: 'error', message: err.message });
-      }
-
-      res.json(JSON.parse(body));
-    }
-  )
 	res.sendFile(path.join(__dirname, "FrontEnd", "build", "index.html"));
 });
 
