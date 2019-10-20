33823d31a493e558cff2bfbe2927ec67cdad8d82
GhostyNZ_13 <gavinj.millar@gmail.com>
2019-10-20 23:05:10 +1300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const path = require("path");
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, "FrontEnd", "build")));
 
 
 // this is our MongoDB database
@@ -36,7 +36,7 @@ const articlesRouter = require('./routes/articles');
 app.use('/articles', articlesRouter);
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+	res.sendFile(path.join(__dirname, "FrontEnd", "build", "index.html"));
 });
 
 // launch our backend into a port
